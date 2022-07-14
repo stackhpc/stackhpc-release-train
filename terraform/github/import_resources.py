72f7f271e5df3e9f75c8f8a8ea7ab35478550e5e
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-14 15:30:44 +0100

    feat: add all resources

--- terraform/github/import_resources.py
+++ terraform/github/import_resources.py
@@ -13,7 +13,6 @@ class Resource:
     components: components
 
     def refresh_resource(self) -> None:
-        print(self.components)
         for ident, index_key in self.components.items():
             query = query_statefile(self.resource_address, ident)
             query_response = unpack_query(query)
@@ -22,8 +21,47 @@ class Resource:
 
 
 class TeamMembership(Resource):
+    # API: gh api \
+    # -H "Accept: application/vnd.github+json" \
+    # /orgs/stackhpc/teams -q '.[] | {id, name}'
     def __init__(self):
-        Resource.__init__(self, "github_team_membership.team_membership", {"6328997:MrJHBauer": None})
+        Resource.__init__(self, "github_team_membership.team_membership",
+            {"6328997:MrJHBauer": "6328997:MrJHBauer"})
+
+
+class TeamRepository(Resource):
+    # API: gh api \
+    # -H "Accept: application/vnd.github+json" \
+    # /orgs/stackhpc/teams -q '.[] | {id, name}'
+    def __init__(self, team_name: str, team_ident: str, repositories: list[str]):
+        Resource.__init__(self, f"github_team_repository.{team_name}_repositories",
+            {f"{team_ident}:{repository}": repository for repository in repositories})
+
+
+class OrganisationTeam(Resource):
+    # API: gh api \
+    # -H "Accept: application/vnd.github+json" \
+    # /orgs/stackhpc/teams -q '.[] | {id, name}'
+    def __init__(self, teams: components):
+        Resource.__init__(self, "github_team.organisation_teams", teams)
+
+
+class BranchProtection(Resource):
+    def __init__(self, team_name: str, branch_pattern: str, repositories: list[str]):
+        Resource.__init__(self, f"github_branch_protection.{team_name}_branch_protection",
+            {f"{repository}:{branch_pattern}": repository for repository in repositories})
+
+
+class IssueLabel(Resource):
+    def __init__(self, label_name: str, repositories: list[str]):
+        Resource.__init__(self, f"github_issue_label.{label_name.replace('-', '_')}_label",
+            {f"{repository}:{label_name}": repository for repository in repositories})
+
+
+class Repository(Resource):
+    def __init__(self, repositories: list[str]):
+        Resource.__init__(self, "github_repository.repositories",
+            {repository : repository for repository in repositories})
 
 
 class QueryResponse(Enum):
@@ -40,6 +78,7 @@ def query_statefile(resource_address: str, resource_id: str) -> subprocess.Compl
 
 def unpack_query(statefile_response: subprocess.CompletedProcess[str]) -> QueryResponse:
     result: QueryResponse = QueryResponse.UNDEFINED
+    print(statefile_response.stderr.decode())
     if statefile_response.returncode == 0:
         if statefile_response.stdout:
             result = QueryResponse.RESOURCE_FOUND
@@ -59,8 +98,8 @@ def import_missing_resource(resource_address: str, resource_id: str, index_key: 
 
 
 def main() -> None:
-    team = TeamMembership()
-    team.refresh_resource()
+    res = IssueLabel("stackhpc-ci", ["kolla-ansible"])
+    res.refresh_resource()
 
 
 if __name__ == "__main__":
