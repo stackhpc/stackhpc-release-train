a289d534319022c224b71312f315beba9d7c8a5b
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-18 12:37:30 +0100

    fix: use `node_id` for `branch_protection`

--- terraform/github/branches.tf
+++ terraform/github/branches.tf
@@ -1,6 +1,6 @@
 resource "github_branch_protection" "ansible_branch_protection" {
   for_each      = toset(var.repositories["Ansible"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = data.github_repository.repositories[each.key].default_branch
   require_conversation_resolution = true
@@ -17,7 +17,7 @@ resource "github_branch_protection" "ansible_branch_protection" {
   # would still need to get the required approval from reviewers and pass any checks before 
   # being able to merge. Also this should prevent outsiders from pushing to the protected branch, 
   # however, whilst they can open a pull request they shoud not be able to merge that would be 
-  # upto the reviewers or codeowners. 
+  # upto the reviewers or codeowners.
   push_restrictions = [
     resource.github_team.organisation_teams["Developers"].node_id
   ]
@@ -29,7 +29,7 @@ resource "github_branch_protection" "ansible_branch_protection" {
 
 resource "github_branch_protection" "azimuth_branch_protection" {
   for_each      = toset(var.repositories["Azimuth"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = data.github_repository.repositories[each.key].default_branch
   require_conversation_resolution = true
@@ -53,7 +53,7 @@ resource "github_branch_protection" "azimuth_branch_protection" {
 
 resource "github_branch_protection" "batch_branch_protection" {
   for_each      = toset(var.repositories["Batch"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = data.github_repository.repositories[each.key].default_branch
   require_conversation_resolution = true
@@ -77,7 +77,7 @@ resource "github_branch_protection" "batch_branch_protection" {
 
 resource "github_branch_protection" "kayobe_branch_protection" {
   for_each      = toset(var.repositories["Kayobe"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = "stackhpc/**"
   require_conversation_resolution = true
@@ -101,7 +101,7 @@ resource "github_branch_protection" "kayobe_branch_protection" {
 
 resource "github_branch_protection" "openstack_branch_protection" {
   for_each      = toset(var.repositories["OpenStack"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = "stackhpc/**"
   require_conversation_resolution = true
@@ -134,7 +134,7 @@ resource "github_branch_protection" "openstack_branch_protection" {
 
 resource "github_branch_protection" "release_train_branch_protection" {
   for_each      = toset(var.repositories["ReleaseTrain"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = data.github_repository.repositories[each.key].default_branch
   require_conversation_resolution = true
@@ -158,7 +158,7 @@ resource "github_branch_protection" "release_train_branch_protection" {
 
 resource "github_branch_protection" "sms_lab_branch_protection" {
   for_each      = toset(var.repositories["SMSLab"])
-  repository_id = each.key
+  repository_id = data.github_repository.repositories[each.key].node_id
 
   pattern                         = data.github_repository.repositories[each.key].default_branch
   require_conversation_resolution = true
