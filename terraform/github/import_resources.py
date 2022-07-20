cf6ea4a9422c9a059f851f481c7668e15db42d7a
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-20 11:52:43 +0100

    feat: allow for `dry-run` argument

--- terraform/github/import_resources.py
+++ terraform/github/import_resources.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 from dataclasses import dataclass
 from enum import Enum, unique
 from functools import reduce
@@ -16,44 +17,45 @@ components = dict[ident, index_key]
 class Resource:
     resource_address: str
     components: components
+    is_dry_run: bool = False
 
     def refresh_resource(self) -> None:
         for ident, index_key in self.components.items():
             query = query_statefile(self.resource_address, ident)
             query_response = unpack_query(query)
             if query_response == QueryResponse.RESOURCE_MISSING or query_response == QueryResponse.RESOURCE_UNKNOWN:
-                import_missing_resource(self.resource_address, ident, index_key)
+                import_missing_resource(self.resource_address, ident, index_key, self.is_dry_run)
 
 
 class TeamMembership(Resource):
     # API: gh api \
     # -H "Accept: application/vnd.github+json" \
     # /orgs/stackhpc/teams -q '.[] | {id, name}'
-    def __init__(self, team_id: str, roster: list[str]):
+    def __init__(self, team_id: str, roster: list[str], is_dry_run: bool):
         Resource.__init__(self, "github_team_membership.team_membership",
-            {f'{team_id}:{entry}': f'{team_id}:{entry}' for entry in roster})
+            {f'{team_id}:{entry}': f'{team_id}:{entry}' for entry in roster}, is_dry_run)
 
 
 class TeamRepository(Resource):
     # API: gh api \
     # -H "Accept: application/vnd.github+json" \
     # /orgs/stackhpc/teams -q '.[] | {id, name}'
-    def __init__(self, team_name: str, team_ident: str, repositories: list[str]):
+    def __init__(self, team_name: str, team_ident: str, repositories: list[str], is_dry_run: bool):
         Resource.__init__(self, f"github_team_repository.{team_name}_repositories",
-            {f"{team_ident}:{repository}": repository for repository in repositories})
+            {f"{team_ident}:{repository}": repository for repository in repositories}, is_dry_run)
 
 
 class OrganisationTeam(Resource):
     # API: gh api \
     # -H "Accept: application/vnd.github+json" \
     # /orgs/stackhpc/teams -q '.[] | {id, name}'
-    def __init__(self, teams: components):
-        Resource.__init__(self, "github_team.organisation_teams", teams)
+    def __init__(self, teams: components, is_dry_run: bool):
+        Resource.__init__(self, "github_team.organisation_teams", teams, is_dry_run)
 
 
 class BranchProtection(Resource):
-    def __init__(self, team_name: str, patterns: dict[str, str]):
-        Resource.__init__(self, f"github_branch_protection.{team_name}_branch_protection", patterns)
+    def __init__(self, team_name: str, patterns: dict[str, str], is_dry_run: bool):
+        Resource.__init__(self, f"github_branch_protection.{team_name}_branch_protection", patterns, is_dry_run)
 
 
     def refresh_resource(self) -> None:
@@ -61,19 +63,19 @@ class BranchProtection(Resource):
             query = query_statefile(self.resource_address)
             query_response = unpack_query(query)
             if query_response == QueryResponse.RESOURCE_MISSING or query_response == QueryResponse.RESOURCE_UNKNOWN:
-                import_missing_resource(self.resource_address, ident, index_key)
+                import_missing_resource(self.resource_address, ident, index_key, self.is_dry_run)
 
 
 class IssueLabel(Resource):
-    def __init__(self, label_name: str, repositories: list[str]):
+    def __init__(self, label_name: str, repositories: list[str], is_dry_run: bool):
         Resource.__init__(self, f"github_issue_label.{label_name}_label",
-            {f"{repository}:{label_name.replace('_', '-')}": repository for repository in repositories})
+            {f"{repository}:{label_name.replace('_', '-')}": repository for repository in repositories}, is_dry_run)
 
 
 class Repository(Resource):
-    def __init__(self, repositories: list[str]):
+    def __init__(self, repositories: list[str], is_dry_run: bool):
         Resource.__init__(self, "github_repository.repositories",
-            {repository : repository for repository in repositories})
+            {repository : repository for repository in repositories}, is_dry_run)
 
 
 class QueryResponse(Enum):
@@ -126,12 +128,14 @@ def unpack_query(statefile_response: subprocess.CompletedProcess[str]) -> QueryR
     return result
 
 
-def import_missing_resource(resource_address: str, resource_id: str, index_key: str = None) -> None:
+def import_missing_resource(resource_address: str, resource_id: str, index_key: str = None, is_dry_run: bool = False) -> None:
     complete_resource_address = f"{resource_address}[\"{index_key}\"]" if index_key else resource_address
     cmd = ["terraform", "import", complete_resource_address, resource_id]
-    output = subprocess.run(cmd, capture_output=True)
-    print(output.stdout.decode())
-
+    if not is_dry_run:
+        output = subprocess.run(cmd, capture_output=True)
+        print(output.stdout.decode())
+    else:
+        print(f"import_missing_resource(resource_address: {resource_address}, resource_id: {resource_id}, index_key: {index_key}", "=>\n\t\033[1m\033[31m", *cmd, "\033[0;0m", end="\n\n")
 
 def get_default_branches() -> dict[str, str]:
     commands = [
@@ -150,13 +154,22 @@ def populate_repository_data() -> None:
     output = subprocess.run(cmd, capture_output=True)
     print(output.stdout.decode())
 
-
 def read_vars(path: str = "terraform.tfvars.json") -> dict[str, Any]:
     complete_path = Path(__file__).parent.joinpath(path)
     return json.load(open(complete_path, "r"))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--dry-run", action=argparse.BooleanOptionalAction, default=False,
+        help="Do not perform any actions that would result in permanent change to the statefile. \
+            Intended to be used to determine if running would make the intended changes")
+    return parser.parse_args()
+
+
 def main() -> None:
+    parsed_args = parse_args()
+    is_dry_run = parsed_args.dry_run
     terraform_vars = read_vars()
     populate_repository_data()
     default_branches = get_default_branches()
@@ -168,29 +181,29 @@ def main() -> None:
     issue_labels = terraform_vars["labels"]
     for team_id, team_repositories in repositories.items():
         if team_id == TeamID.KAYOBE or team_id == TeamID.OPENSTACK:
-            branch_protection_resource = BranchProtection(team_id.name.lower(), {f"{name}:stackhpc/**": name  for name in team_repositories})
+            branch_protection_resource = BranchProtection(team_id.name.lower(), {f"{name}:stackhpc/**": name  for name in team_repositories}, is_dry_run)
             branch_protection_resource.refresh_resource()
         else:
-            branch_protection_resource = BranchProtection(team_id.name.lower(), {f"{name}:{default_branches[name]}": name  for name in team_repositories})
+            branch_protection_resource = BranchProtection(team_id.name.lower(), {f"{name}:{default_branches[name]}": name  for name in team_repositories}, is_dry_run)
             branch_protection_resource.refresh_resource()
     for team_id, users in team_roster.items():
-        team_membership_resource = TeamMembership(team_id.value, users)
+        team_membership_resource = TeamMembership(team_id.value, users, is_dry_run)
         team_membership_resource.refresh_resource()
     for _, users in team_roster.items():
-        team_membership_resource = TeamMembership(team_id.DEVELOPERS.value, users)
+        team_membership_resource = TeamMembership(team_id.DEVELOPERS.value, users, is_dry_run)
         team_membership_resource.refresh_resource()
     for team_id, team_repositories in repositories.items():
-        team_repository_resource = TeamRepository(team_id.name.lower(), team_id.value, team_repositories)
+        team_repository_resource = TeamRepository(team_id.name.lower(), team_id.value, team_repositories, is_dry_run)
         team_repository_resource.refresh_resource()
     for _, team_repositories in repositories.items():
-        team_repository_resource = TeamRepository(TeamID.DEVELOPERS.name.lower(), TeamID.DEVELOPERS.value, team_repositories)
+        team_repository_resource = TeamRepository(TeamID.DEVELOPERS.name.lower(), TeamID.DEVELOPERS.value, team_repositories, is_dry_run)
         team_repository_resource.refresh_resource()
-    organisation_team_resource = OrganisationTeam({str(team.value): str(team) for team in TeamID})
+    organisation_team_resource = OrganisationTeam({str(team.value): str(team) for team in TeamID}, is_dry_run)
     organisation_team_resource.refresh_resource()
     for issue_label in issue_labels:
-        issue_label_resource = IssueLabel(issue_label, [repository for teams in repositories.values() for repository in teams])
+        issue_label_resource = IssueLabel(issue_label, [repository for teams in repositories.values() for repository in teams], is_dry_run)
         issue_label_resource.refresh_resource()
-    repository_resource = Repository([*itertools.chain.from_iterable(repositories.values())])
+    repository_resource = Repository([*itertools.chain.from_iterable(repositories.values())], is_dry_run)
     repository_resource.refresh_resource()
 
 
