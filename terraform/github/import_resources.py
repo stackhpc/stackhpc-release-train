9fd9e5ea2e0cd623253ffc1245b4399ceae8132e
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-29 03:05:55 +0100

    feat: improve `get_default_branches`

--- terraform/github/import_resources.py
+++ terraform/github/import_resources.py
@@ -160,12 +160,16 @@ def import_missing_resource(resource_address: str, resource_id: str, index_key: 
 def get_default_branches() -> dict[str, str]:
     branches = {}
     cmd = ["terraform", "state", "pull"]
-    complete_path = Path(__file__).parent.joinpath("terraform.state.pull")
-    tfstate_file = open(complete_path, "w", encoding="utf-8")
-    subprocess.run(cmd, stdout=tfstate_file, encoding="utf-8", check=False)
-    tfstate_json = json.load(open(complete_path, "r", encoding="utf-8"))
-    for repository in tfstate_json["resources"][0]["instances"]:
-        branches[repository["index_key"]] = repository["attributes"]["default_branch"]
+    print("Fetching latest remote state file =>", *cmd)
+    output = subprocess.run(cmd, capture_output=True, check=False)
+    try:
+        output.check_returncode()
+    except subprocess.CalledProcessError:
+        print(output.stderr.decode())
+    else:
+        state_file = json.loads(output.stdout.decode())
+        for repository in state_file["resources"][0]["instances"]:
+            branches[repository["index_key"]] = repository["attributes"]["default_branch"]
     return branches
 
 
