5c55fe9aea61297c518b6e8faffa3cb5d9df4887
Pierre Riteau <pierre@stackhpc.com>
2026-03-11 12:20:08 +0100

    Bump integrations/github to v6
    Remove use of deprecated github_repository attributes:
    
    - default_branch
    - ignore_vulnerability_alerts_during_read
    - private
    
    Ignore allow_update_branch which is enabled on some repositories.
    
    Ignore has_discussions for stackhpc/ansible-collection-cephadm.
    
    Change push_restrictions to restrict_pushes.
    
    Import ansible-slurm-appliance required status checks.

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -8,10 +8,11 @@ resource "github_repository" "repositories" {
   allow_auto_merge       = true
   lifecycle {
     ignore_changes = [
+      allow_update_branch,
       description,
       homepage_url,
-      private,
       visibility,
+      has_discussions,
       has_issues,
       has_projects,
       has_wiki,
@@ -20,14 +21,12 @@ resource "github_repository" "repositories" {
       auto_init,
       gitignore_template,
       license_template,
-      default_branch,
       archived,
       archive_on_destroy,
       pages,
       topics,
       template,
       vulnerability_alerts,
-      ignore_vulnerability_alerts_during_read
     ]
     prevent_destroy = true
   }
